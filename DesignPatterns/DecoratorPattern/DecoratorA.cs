8473d1c136130281583a6981ec5894946a4ee1c8
Chandru Kuppuswamy <chandrukuppuswamy@outlook.com>
2022-05-28 23:43:36 +0530

    Initial commit

---
+++ DesignPatterns/DecoratorPattern/DecoratorA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    internal class DecoratorA : IComponent
+    {
+        IComponent component;
+
+        internal DecoratorA(IComponent c)
+        {
+            this.component = c;
+        }
+        public string Operation()
+        {
+            string s = component.Operation();
+            s += "and listening to Classic FM ";
+            return s;
+        }
+    }
+
+
+
+}
+
