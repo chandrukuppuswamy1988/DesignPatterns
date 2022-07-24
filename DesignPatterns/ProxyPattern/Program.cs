3c0ce7176d41a41475c27b9e28ada88860e8ff34
Chandru Kuppuswamy <chandrukuppuswamy@outlook.com>
2022-07-24 10:32:41 +0530

    Proxy Pattern Changes

---
+++ DesignPatterns/ProxyPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using ProxyPattern;
+
+Console.WriteLine("Proxy Pattern\n");
+
+ISubject subject = new Proxy();
+
+Console.WriteLine(subject.Request());
+Console.WriteLine(subject.Request());
+
+subject = new ProtectionProxy();
+Console.WriteLine(subject.Request());
+Console.WriteLine((subject as ProtectionProxy).Authenticate("Secret"));
+Console.WriteLine((subject as ProtectionProxy).Authenticate("Abracadabra"));
+Console.WriteLine(subject.Request());
+  
---
+++ DesignPatterns/ProxyPattern/Proxy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public class Proxy : ISubject
+    {
+        Subject subject;
+        public string Request()
+        {
+            if (subject == null) {
+                Console.WriteLine("Subject inactive");
+                subject = new Subject();
+            }
+            Console.WriteLine("Subject active");
+            return "Proxy: call to " + subject.Request();
+        }
+    }
+}
