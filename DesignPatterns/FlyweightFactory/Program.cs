e43e53d450c649d28ffd6661332a5e32df3a4237
Chandru Kuppuswamy <chandrukuppuswamy@outlook.com>
2022-08-14 11:59:21 +0530

    singleton instance

--- DesignPatterns/FlyweightFactory/Program.cs
+++ DesignPatterns/FlyweightFactory/Program.cs
@@ -3,15 +3,20 @@ using FlyweightFactory;
 
 int extrinsicstate = 22;
 var factory = new FlyweightFactory.FlyweightFactory();
+
 // Work with different flyweight instances
 Flyweight fx = factory.GetFlyweight("X");
 fx.Operation(--extrinsicstate);
+
 Flyweight fy = factory.GetFlyweight("Y");
 fy.Operation(--extrinsicstate);
+
 Flyweight fz = factory.GetFlyweight("Z");
 fz.Operation(--extrinsicstate);
+
 UnsharedConcreteFlyweight fu = new
     UnsharedConcreteFlyweight();
 fu.Operation(--extrinsicstate);
+
 // Wait for user
 Console.ReadKey();
