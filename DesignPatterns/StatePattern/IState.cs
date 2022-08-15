bbe55f7d8e622b32381f600f49e028c7acfc4c2b
Chandru Kuppuswamy <chandrukuppuswamy@outlook.com>
2022-08-15 15:45:01 +0530

    StatePattern and StatergyPattern

---
+++ DesignPatterns/StatePattern/IState.cs
@@ -0,0 +1,17 @@
+﻿using StrategyPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    public interface IState
+    {
+        int MoveUp(Context context);
+        int MoveDown(Context context);
+
+
+    }
+}
---
+++ DesignPatterns/StatePattern/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StatePattern;
+
+
+Context context = new Context();
+
+context.State = new NormalState();
+Random r = new Random(37);
+
+for (int i = 5; i <= 25; i++)
+{
+    int command = r.Next(3);
+    Console.Write(context.Request(command) + " ");
+}
+
+Console.WriteLine();
+
